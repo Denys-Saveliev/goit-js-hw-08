fca5079d75e33a61a107caed501502caddd236cd
Denys-Saveliev <denys.saveliev@ukr.net>
2022-02-18 14:21:35 +0200

    update task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+const _ = require('lodash');
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+const formRef = document.querySelector('.feedback-form');
+
+formRef.addEventListener('input', _.throttle(onUpdateInput, 500));
+formRef.addEventListener('submit', onFormSubmit);
+try {
+   let fieldText = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+   formRef.elements.email.value = fieldText.email;
+   formRef.elements.message.value = fieldText.message;
+} catch {}
+
+function onUpdateInput(e) {   
+   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(createFieldStatusObject()));
+}
+
+function createFieldStatusObject() {
+  return {
+    email: formRef.elements.email.value,
+    message: formRef.elements.message.value,
+  };
+}
+
+function onFormSubmit(e) {
+   e.preventDefault();
+   console.log(createFieldStatusObject());
+   formRef.reset();
+   localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
 
